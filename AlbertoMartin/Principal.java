c6f04b5aa749863222bff036eb9bcf2238a11dce
ismael-feito <isfemo27@gmail.com>
2025-04-22 19:36:39 +0200

    Añadido cálculo de MCD

--- AlbertoMartin/Principal.java
+++ AlbertoMartin/Principal.java
@@ -5,22 +5,20 @@ import java.util.Scanner;
 public class Principal {
 
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.println("Introduzca el número sobre el que hacer los cálculos");
-        
+
         int numero;
         try {
             numero = sc.nextInt();
         } catch (Exception e) {
             System.out.println("El dato introducido no es un número");
             return;
-        }        
-        
+        }
+
+        // Usamos el nuevo método refactorizado
         CalculosMatematicos misCalculosMatematicos = new CalculosMatematicos();
-        
         misCalculosMatematicos.calculosRefactorizados(numero);
     }
 }
-
