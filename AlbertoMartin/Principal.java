3736fa8fa233ca5fe7cda924c1e93b50644edefb
ismael-feito <isfemo27@gmail.com>
2025-04-22 19:30:51 +0200

    Resuelto conflicto al fusionar RamaFactorizar

--- AlbertoMartin/Principal.java
+++ AlbertoMartin/Principal.java
@@ -4,22 +4,23 @@ import java.util.Scanner;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Introduzca el número sobre el que hacer los cálculos");
-		
-		int numero;
-		try {
-		numero = sc.nextInt();
-		}catch (Exception e){
-		System.out.println("El dato introducido no es un número");
-		return;
-		}		
-		new CalculosMatematicos().calculos(numero);
-
-	}
-
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Introduzca el número sobre el que hacer los cálculos");
+        
+        int numero;
+        try {
+            numero = sc.nextInt();
+        } catch (Exception e) {
+            System.out.println("El dato introducido no es un número");
+            return;
+        }        
+        
+        CalculosMatematicos misCalculosMatematicos = new CalculosMatematicos();
+        
+        misCalculosMatematicos.calculosRefactorizados(numero);
+    }
 }
+
